6dfb5c0d4d23073fd742fcd377d371417ece004e
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 23:03:32 -0400

    Base API Inventario

---
+++ src/main/java/com/api/apiinventario/ApiinventarioApplication.java
@@ -0,0 +1,13 @@
+package com.api.apiinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiinventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiinventarioApplication.class, args);
+	}
+
+}
